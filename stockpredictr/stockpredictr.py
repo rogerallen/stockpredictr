00db7fe225fb4d5065253e4bc1a29012d9bf1aa9
rallen <rallen@c17856c4-a0e3-11de-be8d-735280eae207>
2011-02-17 06:49:41 +0000

    fix for issue 27 to explicitly go to django 1.2 this caused a few minor issues with text that needed to be un-escaped. changed version so I can rollback if necessary. (Version 7).
    git-svn-id: https://stockpredictr.googlecode.com/svn/trunk@56 c17856c4-a0e3-11de-be8d-735280eae207

--- stockpredictr/stockpredictr.py
+++ stockpredictr/stockpredictr.py
@@ -20,6 +20,17 @@
 stockpredictr.py - main code for handling the http://stockpredictr.appspot.com/
 website via google's app engine system.
 """
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# even though I don't use django, apparently I need to pick the version
+# see http://code.google.com/appengine/docs/python/tools/libraries.html#Django
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+#use_library('django', '0.96')
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from stockpredictr_views  import *
